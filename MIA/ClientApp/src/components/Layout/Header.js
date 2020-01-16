2b179eba4a4a659cad15e5540caef4de028a2fa5
mustafa <mustafa.magdy1@gmail.com>
2020-01-16 18:18:33 +0200

    toggle menu, and search form

--- MIA/ClientApp/src/components/Layout/Header.js
+++ MIA/ClientApp/src/components/Layout/Header.js
@@ -1,52 +1,85 @@
 import React from "react";
+import classNames from "classnames";
 
-const Header = ({ props }) => {
-  return (
-    <React.Fragment>
-      <header className="fixed">
-        <div className="logo">
-          <a href="#" title="#">
-            <img src="assets/images/logo.png" alt="#" />
-          </a>
-        </div>
-        {/* <!-- end logo --> */}
-        <div className="mainmenu">
-          <ul>
-            <li>
-              <a href="#" title="#">
-                Awards
-              </a>
-            </li>
-            <li>
-              <a href="#" title="#">
-                About Us
-              </a>
-            </li>
-            <li>
-              <a href="#" title="#">
-                News
-              </a>
-            </li>
-            <li>
-              <a href="#" title="#">
-                Program
-              </a>
-            </li>
-            <li>
-              <a href="#" title="#">
-                Gallery
-              </a>
-            </li>
-            <li>
-              <a href="#" title="#">
-                Contact US
-              </a>
-            </li>
-          </ul>
-        </div>
-      </header>
-    </React.Fragment>
-  );
-};
+class Header extends React.Component {
+  state = {
+    headerFixed: false
+  };
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll, { passive: true });
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
+  }
+
+  handleScroll = () => {
+    //calculate the percentage where we should accept the gdpr if we scroll to without clicking accept explicitly
+    // (top / height) - height * 100
+    let rootNode = document.documentElement,
+      body = document.body,
+      top = "scrollTop",
+      height = "scrollHeight";
+
+    let percentage = ((rootNode[top] || body[top]) / ((rootNode[height] || body[height]) - rootNode.clientHeight)) * 100;
+
+    console.log(percentage);
+
+    if (percentage > 0) {
+      this.setState({ headerFixed: true });
+    } else {
+      this.setState({ headerFixed: false });
+    }
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <header className={classNames({ fixed: this.state.headerFixed })}>
+          <div className="logo">
+            <a href="#" title="#">
+              <img src="assets/images/logo.png" alt="#" />
+            </a>
+          </div>
+          {/* <!-- end logo --> */}
+          <div className="mainmenu">
+            <ul>
+              <li>
+                <a href="#" title="#">
+                  Awards
+                </a>
+              </li>
+              <li>
+                <a href="#" title="#">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" title="#">
+                  News
+                </a>
+              </li>
+              <li>
+                <a href="#" title="#">
+                  Program
+                </a>
+              </li>
+              <li>
+                <a href="#" title="#">
+                  Gallery
+                </a>
+              </li>
+              <li>
+                <a href="#" title="#">
+                  Contact US
+                </a>
+              </li>
+            </ul>
+          </div>
+        </header>
+      </React.Fragment>
+    );
+  }
+}
 
 export default Header;
--- MIA/ClientApp/src/components/Layout/index.js
+++ MIA/ClientApp/src/components/Layout/index.js
@@ -3,14 +3,30 @@ import Header from "./Header";
 import Footer from "./Footer";
 import UserProvider from "containers/Providers/UserProvider";
 import Sidebar from "./Sidebar";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import appActions from "store/app/actions";
 
 class Layout extends React.PureComponent {
+  dismissDlgs = event => {
+    if (event.keyCode === 27) {
+      const { toggleSearchForm } = this.props;
+      toggleSearchForm();
+    }
+  };
+
+  componentDidMount() {
+    document.addEventListener("keydown", this.dismissDlgs, false);
+  }
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.dismissDlgs, false);
+  }
   render() {
     return (
       <UserProvider>
         <section id="main_site">
           <aside>sdsdsd</aside>
-          <div className="search_modal">
+          <div id="search_modal" className="search_modal">
             <form action="#">
               <input type="text" placeholder="Search ..." />
               <button type="submit">
@@ -28,4 +44,5 @@ class Layout extends React.PureComponent {
   }
 }
 
-export default Layout;
+const mapDispatchToProps = dispatch => bindActionCreators({ ...appActions }, dispatch);
+export default connect(null, mapDispatchToProps)(Layout);
