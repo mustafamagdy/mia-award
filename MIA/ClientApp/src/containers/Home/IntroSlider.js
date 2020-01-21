cb3b217782b003d3e827bf9479f0d67e73e704b1
mustafa <mustafa.magdy1@gmail.com>
2020-01-21 17:05:01 +0200

    wip intro slider

--- MIA/ClientApp/src/containers/Home/IntroSlider.js
+++ MIA/ClientApp/src/containers/Home/IntroSlider.js
@@ -1,48 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { Trans } from "@lingui/macro";
 
 import "sass/intro.scss";
 
 const Intro = props => {
+  const [current, setCurrent] = useState(0);
+  const [startAnim, setStartAnim] = useState(false);
+  const items = [
+    {
+      key: 1,
+      keyText: "01",
+      title: "slider Title 01",
+      text: `01 - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+      smallImgPath: "assets/images/big_image.png",
+      bigImgPath: "assets/images/big_image.png"
+    },
+    {
+      key: 2,
+      keyText: "02",
+      title: "slider Title 02",
+      text: `02 - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+      smallImgPath: "assets/images/small_image.png",
+      bigImgPath: "assets/images/small_image.png"
+    },
+    {
+      key: 3,
+      keyText: "03",
+      title: "slider Title 03",
+      text: `03 - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+      smallImgPath: "assets/images/big_image.png",
+      bigImgPath: "assets/images/big_image.png"
+    },
+    {
+      key: 4,
+      keyText: "04",
+      title: "slider Title 04",
+      text: `04 - Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+      dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+      book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
+      unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
+      recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`,
+      smallImgPath: "assets/images/small_image.png",
+      bigImgPath: "assets/images/small_image.png"
+    }
+  ];
   return (
     <section id="intro">
       <div className="container">
         <div className="slide_item">
-          <div className="view_award">
-            <div className="big_image">
-              <a href="#" title="#">
-                <img src="assets/images/big_image.png" alt="#" />
-              </a>
-              <div className="progress_bar" style={{ width: 460 }}></div>
-            </div>
+          <TransitionGroup className="view_award">
+            <CSSTransition key={items[current].key} appear={true} timeout={1000} classNames="big_image">
+              <div className="">
+                <a href="#" title="#">
+                  <img src={items[current].bigImgPath} alt="#" />
+                </a>
+              </div>
+            </CSSTransition>
+            <div className="progress_bar" style={{ width: 460 }}></div>
             <div className="desc">
-              <span>slider Title</span>
-              <p>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-                dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
-                book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-              </p>
+              <span>{items[current].title}</span>
+              <p>{items[current].text}</p>
               <a href="#" title="#">
                 <Trans id="view_awards">view awards</Trans>
               </a>
             </div>
-          </div>
+          </TransitionGroup>
         </div>
         <div className="slider_thumb">
+          <button type="button" className="arrow arrow_prev" onClick={() => setCurrent(current > 0 ? current - 1 : items.length - 1)}>
+            <i className="icofont-simple-left"></i>
+          </button>
           <div className="thmb">
-            <img src="assets/images/small_image.png" alt="#" />
+            <img src={items.length > current + 1 ? items[current + 1].smallImgPath : items[0].smallImgPath} alt="#" />
           </div>
-          <button type="button" className="arrow_next">
+          <button type="button" className="arrow arrow_next" onClick={() => setCurrent(current < items.length - 1 ? current + 1 : 0)}>
             <i className="icofont-simple-right"></i>
           </button>
         </div>
         <div className="slider_dots">
-          <span className="current">01</span>
-          <span>02</span>
-          <span>03</span>
-          <span>04</span>
+          {items.map((item, index) => (
+            <span key={item.key} className={index == current ? "current" : ""}>
+              {item.keyText}
+            </span>
+          ))}
         </div>
       </div>
     </section>
