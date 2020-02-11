7ad39f0f44f16e4c2d6e367962918f0ef88cf971
mustafa <mustafa.magdy1@gmail.com>
2020-02-11 13:59:26 +0200

    protected route

--- MIA/ClientApp/src/containers/Members/index.js
+++ MIA/ClientApp/src/containers/Members/index.js
@@ -1,21 +1,46 @@
 import React from "react";
-import { Route } from "react-router";
+import { Route, withRouter, Switch, Redirect } from "react-router";
+import { connect } from "react-redux";
+import membersActions from "store/members/actions";
+import { bindActionCreators } from "redux";
+
 import Artworks from "./Artworks";
 import Artwork from "./Artworks/Artwork";
 import MemberSidebar from "./MemberSidebar";
 import Profile from "./Profile";
+import Auth from "./Auth";
 
-const MemberArea = props => (
-  <section id="member_area">
-    <div className="container">
-      <MemberSidebar />
-      <div className="member_wrapper">
-        <Route exact path="/members" component={Artworks} />
-        <Route exact path="/members/profile" component={Profile} />
-        <Route path="/members/artwork" component={Artwork} />
-      </div>
-    </div>
-  </section>
-);
+const MemberArea = ({ location, match, currentUser, isLoggedIn, ...props }) => {
+  if (location.pathname === "/members") {
+    if (!isLoggedIn || !currentUser) {
+      return <Redirect to={"/members/signin"} />;
+    } else {
+      return (
+        <section id="member_area">
+          <div className="container">
+            <MemberSidebar />
+            <div className="member_wrapper">
+              <Switch>
+                <Route exact path="/members" component={Artworks} />
+                <Route exact path="/members/profile" component={Profile} />
+                <Route path="/members/artwork" component={Artwork} />
+              </Switch>
+            </div>
+          </div>
+        </section>
+      );
+    }
+  } else if (location.pathname === "/members/signin") {
+    return (
+      <section id="member_area">
+        <div className="container">
+          <Auth />
+        </div>
+      </section>
+    );
+  }
+};
 
-export default MemberArea;
+const mapStateToProps = ({ auth: { currentUser, isLoggedIn } }) => ({ currentUser, isLoggedIn });
+const mapDispatchToProps = dispatch => bindActionCreators({ ...membersActions }, dispatch);
+export default connect(mapStateToProps, mapDispatchToProps)(withRouter(MemberArea));
