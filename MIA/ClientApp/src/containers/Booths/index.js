daedc6970273a4eb67e12416b3c9ab61d151365c
mustafa <mustafa.magdy1@gmail.com>
2020-02-21 16:38:48 +0200

    wip booths

--- MIA/ClientApp/src/containers/Booths/index.js
+++ MIA/ClientApp/src/containers/Booths/index.js
@@ -175,39 +175,69 @@ const Payment = ({ active, ...props }) => {
     <div className={classNames("tab_item payment_tab", { active })}>
       <div className="paymnets_area">
         <div className="title">Choose Your Payment Method :</div>
-        <div className="choose_area">
-          <label htmlFor="payOnline">
-            <span>Pay Online</span>
-            <input type="radio" id="payOnline" name="customRadio" />
-            <div className="checkmark"></div>
-          </label>
-          <div className="pay_online_form">
-            <div className="pay_icons">
-              <span>
-                <img src="assets/images/pay1.png" />
-              </span>
-              <span>
-                <img src="assets/images/pay2.png" />
-              </span>
-              <span>
-                <img src="assets/images/pay3.png" />
-              </span>
-              <span>
-                <img src="assets/images/pay4.png" />
-              </span>
-            </div>
+        <BlockUi tag="div" blocking={!awardConfirmed} className={classNames("pay_col_two", { active: awardConfirmed })}>
+          <label>Choose Your Payment Method :</label>
+          <div className="choose_area">
+            <label htmlFor="online">
+              <span>Pay Online</span>
+              <input
+                ref={register}
+                type="radio"
+                id="online"
+                value="online"
+                name="payment.paymentMethod"
+                onChange={setPaymentMethod}
+                checked={useOnlinePayment}
+              />
+              <div className="checkmark"></div>
+            </label>
+            <BlockUi tag="div" blocking={!useOnlinePayment} className={classNames("pay_online_form", { move: !useOnlinePayment })}>
+              <img src="/assets/images/pay_logo.png" />
+              <PaymentForm
+                cardTokenized={token => {
+                  setPaymentToken(token);
+                }}
+              />
+              <div className="confirm">
+                <button id="pay-button" type="submit" form="payment-form">
+                  Pay & Continue
+                </button>
+              </div>
+            </BlockUi>
           </div>
-        </div>
-        <div className="choose_area">
-          <label htmlFor="payOffline">
-            <span>Pay Offline</span>
-            <input type="radio" id="payOffline" name="customRadio" />
-            <div className="checkmark"></div>
-          </label>
-          <div className="pay_offline_form">
-            <p>please upload the reciept to be approved from the adminstration and confirm your payment</p>
+          <div className="choose_area">
+            <label htmlFor="offline">
+              <span>Pay Offline</span>
+              <input
+                ref={register}
+                type="radio"
+                id="offline"
+                value="offline"
+                name="payment.paymentMethod"
+                onChange={setPaymentMethod}
+                checked={!useOnlinePayment}
+              />
+              <div className="checkmark"></div>
+            </label>
+            <BlockUi tag="div" blocking={useOnlinePayment} className={classNames("pay_offline_form", { move: useOnlinePayment })}>
+              <p>please upload the reciept to be approved from the adminstration and confirm your payment</p>
+              <form id="offline-payment" onSubmit={onPayment}>
+                <input ref={register} name="payment.receiptAmount" type="text" placeholder="Amount" />
+                <input ref={register} name="payment.receiptNumber" type="text" placeholder="Transaction Number" />
+                <input ref={register} name="payment.receiptDate" type="text" placeholder="Payment Date" />
+                <div className="confirm">
+                  <input type="file" id="receipt" name="payment.receipt" ref={register} />
+                  <label htmlFor="receipt" className="btn-2">
+                    Choose receipt image
+                  </label>
+                  <button form="offline-payment" type="submit">
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </BlockUi>
           </div>
-        </div>
+        </BlockUi>
         <div className="Upload">
           <input type="file" id="file" />
           <label htmlFor="file" className="btn-2">
