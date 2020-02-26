6861fa77536011986fa6762572f41aeb810876fe
mustafa <mustafa.magdy1@gmail.com>
2020-02-26 07:55:57 +0200

    wip photos slider

--- MIA/ClientApp/src/containers/Home/PhotoAlbum.js
+++ MIA/ClientApp/src/containers/Home/PhotoAlbum.js
@@ -6,6 +6,8 @@ import { connect } from "react-redux";
 import homeActions from "store/home/actions";
 import { LanguageContext } from "containers/Providers/LanguageProvider";
 import "utils";
+import Swiper from "react-id-swiper";
+import "swiper/css/swiper.css";
 
 const PhotoAlbum = ({ mainAlbum, ...props }) => {
   const [allItems, setAllItems] = useState([]);
@@ -16,7 +18,7 @@ const PhotoAlbum = ({ mainAlbum, ...props }) => {
   useEffect(() => {
     if (mainAlbum && mainAlbum.items) {
       setAllItems(mainAlbum.items);
-      setSliderItems(mainAlbum.items.slice(0, 2));
+      setSliderItems(mainAlbum.items);
     }
   }, [mainAlbum]);
 
@@ -24,17 +26,42 @@ const PhotoAlbum = ({ mainAlbum, ...props }) => {
     setCurrentItem(sliderItems[1]);
   }, [sliderItems]);
 
-  const previous = () => {
-    let _a = activeIndex;
-    if (_a < 1) _a = sliderItems.length - 1;
-    else _a = _a - 1;
-    setActiveIndex(_a);
-  };
-  const next = () => {
-    let _a = activeIndex;
-    if (_a >= sliderItems.length - 1) _a = 0;
-    else _a = _a + 1;
-    setActiveIndex(_a);
+  // const previous = () => {
+  //   let _a = activeIndex;
+  //   if (_a < 1) _a = sliderItems.length - 1;
+  //   else _a = _a - 1;
+  //   setActiveIndex(_a);
+  // };
+  // const next = () => {
+  //   let _a = activeIndex;
+  //   if (_a >= sliderItems.length - 1) _a = 0;
+  //   else _a = _a + 1;
+  //   setActiveIndex(_a);
+  // };
+
+  const params = {
+    slidesPerView: 1,
+    slidesPerGroup: 2,
+    spaceBetween: 0,
+    loop: true,
+    simulateTouch: true,
+    // getSwiper: swiper => {
+    //   setSwiper(swiper);
+    // },
+    breakpoints: {
+      991: {
+        // slidesPerView: 2.5,
+        spaceBetween: 20,
+        simulateTouch: false,
+        loop: true
+      }
+    },
+    navigation: {
+      nextEl: "#next_album_item",
+      prevEl: "#prev_album_item"
+    }
+    // slideClass: "item",
+    // slideActiveClass: "current"
   };
 
   return currentItem === undefined ? null : (
@@ -42,7 +69,7 @@ const PhotoAlbum = ({ mainAlbum, ...props }) => {
       <div className="container">
         <div className="big_show">
           <div className="imgthumb">
-            <img src={currentItem.mediaType == "image"? currentItem.fileUrl: currentItem.posterUrl} />
+            <img src={currentItem.mediaType == "image" ? currentItem.fileUrl : currentItem.posterUrl} />
           </div>
         </div>
         <div className="videos_slider">
@@ -55,28 +82,29 @@ const PhotoAlbum = ({ mainAlbum, ...props }) => {
           <LanguageContext.Consumer>
             {({ locale }) => (
               <div className="slider_media">
-                {sliderItems.map((s, i) => {
-                  console.log("item ", s);
-                  const isCurrent = currentItem.id == s.id;
-                  const currentItemTitlePart1 = s.title[locale.code].split(" ")[0];
-                  const currentItemTitlePart2 = s.title[locale.code].split(" ").shift();
+                <Swiper {...params}>
+                  {sliderItems.map((s, i) => {
+                    // const isCurrent = currentItem.id == s.id;
+                    const currentItemTitlePart1 = s.title[locale.code].split(" ")[0];
+                    const currentItemTitlePart2 = s.title[locale.code].split(" ").shift();
 
-                  return (
-                    <div key={s.id} className={classNames("item", { current: isCurrent })}>
-                      <span>
-                        {currentItemTitlePart1 && <i>{currentItemTitlePart1}</i>} {currentItemTitlePart2 && currentItemTitlePart2}
-                      </span>
-                      <div className="imgthumb">
-                        <img src={s.mediaType == "image"? s.fileUrl: s.posterUrl} />
+                    return (
+                      <div key={s.id}>
+                        <span>
+                          {currentItemTitlePart1 && <i>{currentItemTitlePart1}</i>} {currentItemTitlePart2 && currentItemTitlePart2}
+                        </span>
+                        <div className="imgthumb">
+                          <img src={s.mediaType == "image" ? s.fileUrl : s.posterUrl} />
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </Swiper>
                 <div className="slide_nav">
-                  <button type="button" className="arrow_prev" onClick={previous}>
+                  <button type="button" className="arrow_prev" id="prev_album_item">
                     <i className="icofont-simple-left"></i>
                   </button>
-                  <button type="button" className="arrow_next" onClick={next}>
+                  <button type="button" className="arrow_next" id="next_album_item">
                     <i className="icofont-simple-right"></i>
                   </button>
                 </div>
