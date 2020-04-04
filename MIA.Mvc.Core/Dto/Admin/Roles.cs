bd7a1b70ec822724f07087fb6484f810c041457b
MMagdy <mustafa.magdy@live.com>
2020-04-04 23:48:51 +0300

    validate system roles

--- MIA.Mvc.Core/Dto/Admin/Roles.cs
+++ MIA.Mvc.Core/Dto/Admin/Roles.cs
@@ -4,6 +4,7 @@ namespace MIA.Dto.Admin {
   public class RoleDto : BaseResponse {
     public string Name { get; set; }
     public string Id { get; set; }
+    public bool SystemRole { get; set; }
   }
 
 }
