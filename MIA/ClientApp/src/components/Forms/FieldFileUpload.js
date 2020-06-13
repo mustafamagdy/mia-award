a61ff0d59651748e5dc3a3921065f7a71253afd2
MMagdy <mustafa.magdy@live.com>
2020-06-13 15:07:59 +0300

    file upload validation

--- MIA/ClientApp/src/components/Forms/FieldFileUpload.js
+++ MIA/ClientApp/src/components/Forms/FieldFileUpload.js
@@ -1,13 +1,16 @@
 import React from "react";
 import { ErrorMessage } from "../Forms";
+import { getPropValue } from "utils";
 
 const FieldFilUplpad = ({
-  form: { setFieldValue },
-  field: { name, hasError },
+  form: { setFieldValue, errors },
+  field: { name },
+  hasError,
   ...props
 }) => {
+  console.log(name, hasError, errors, errors[name]);
   return (
-    <>
+    <div className="form-group__wrap  col-6 col-md-6 col-sm-12">
       <input
         id={name}
         name={name}
@@ -17,8 +20,13 @@ const FieldFilUplpad = ({
         }}
         {...props}
       />
-      <ErrorMessage name={name} hasError={hasError} />
-    </>
+      <ErrorMessage
+        name={name}
+        hasError={hasError}
+        isFile={true}
+        error={getPropValue(errors, name)}
+      />
+    </div>
   );
 };
 
