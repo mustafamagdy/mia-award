25a5f9ce7bfb30c156de1909f7b8837a9736f41b
MMagdy <mustafa.magdy@live.com>
2020-07-04 13:49:02 +0300

    update entities

---
+++ MIA.ORMContext/Migrations/20200704104748_UpdateJudgeVoteEntities.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MIA.ORMContext.Migrations
+{
+    public partial class UpdateJudgeVoteEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JudgeComplete",
+                table: "JudgeVotes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "VotingValue",
+                table: "JudgeVotes",
+                nullable: true,
+                oldClrType: typeof(int));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "VotingValue",
+                table: "JudgeVotes",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "JudgeComplete",
+                table: "JudgeVotes",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
