95054217dbf64f47d8cf4694bf6b660c1dd855cd
MMagdy <mustafa.magdy1@gmail.com>
2021-06-12 17:23:41 +0200

    initial commit

---
+++ MIA/ClientApp/build/precache-manifest.8dd76686e19eab9c969049c9e4bd7118.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2db1d7308160c72e7c34390b666c0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1e50e0609a3b49edb17",
+    "url": "/static/css/common.b12ac318.chunk.css"
+  },
+  {
+    "revision": "4fee115b768b0fb3a0a3",
+    "url": "/static/css/main.2b81136c.chunk.css"
+  },
+  {
+    "revision": "4f84040a084aaa71f3c3",
+    "url": "/static/css/react.4ea9b401.chunk.css"
+  },
+  {
+    "revision": "52e2d56f1cbd8b9621da",
+    "url": "/static/js/4.7251fd7a.chunk.js"
+  },
+  {
+    "revision": "d0ce78aab8b762167f4c",
+    "url": "/static/js/5.61d79a23.chunk.js"
+  },
+  {
+    "revision": "56dce5d01f2ecea99fbc",
+    "url": "/static/js/6.4c48a1b5.chunk.js"
+  },
+  {
+    "revision": "d1e50e0609a3b49edb17",
+    "url": "/static/js/common.9d23157c.chunk.js"
+  },
+  {
+    "revision": "4fee115b768b0fb3a0a3",
+    "url": "/static/js/main.fd5e3dcc.chunk.js"
+  },
+  {
+    "revision": "4f84040a084aaa71f3c3",
+    "url": "/static/js/react.e51e8a39.chunk.js"
+  },
+  {
+    "revision": "496fcbc69dc5161220ee",
+    "url": "/static/js/runtime~main.f85d14b6.js"
+  },
+  {
+    "revision": "1bd9eea8f0acd226c502ecf80d294d16",
+    "url": "/static/media/messages.1bd9eea8.po"
+  },
+  {
+    "revision": "483cc7fdb5f066ae174bcc2886fde4be",
+    "url": "/static/media/messages.483cc7fd.po"
+  }
+]);
---
+++ MIA/ClientApp/build/static/js/6.4c48a1b5.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{1314:function(n,w){}}]);
